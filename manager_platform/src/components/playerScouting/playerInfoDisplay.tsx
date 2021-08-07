ec8ccef2e5d0bfa9c2915081a1b2d3294bb787f9
tylersmall19 <tylersmall.user@gmail.com>
2021-08-07 17:21:18 -0500

    Display all player traits on the list

--- manager_platform/src/components/playerScouting/playerInfoDisplay.tsx
+++ manager_platform/src/components/playerScouting/playerInfoDisplay.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { styleStatNumber } from '../../helpers/formatting/statFormattingHelper';
 import { PlayerInfo } from '../../types/playerInfo';
 import styles from './playerInfoDisplay.module.css';
 
@@ -9,7 +10,47 @@ type PlayerProps = {
 export const PlayerInfoDisplay = ({player}: PlayerProps) => {
   return (
     <div className={styles.playerContainer}>
-      <h3 className={styles.playerHeader}>{player.firstName} "{player.ign}" {player.lastName}</h3>
+      <h3 className={styles.playerHeader}>{player.firstName} "{player.ign}" {player.lastName} [{player.region}] [OVR: {styleStatNumber(player.overall)}]</h3>
+      
+      <hr />
+
+      <span className={styles.playerStat}>
+        Nerves: {styleStatNumber(player.nerves, false)}
+      </span>
+      |
+      <span className={styles.playerStat}>
+        Ego: {styleStatNumber(player.ego, false)}
+      </span>
+      
+      <br />
+
+      <span className={styles.playerStat}>
+        Proactivity: {styleStatNumber(player.proactive)}
+      </span>
+
+      <span className={styles.playerStat}>
+        Mechanics: {styleStatNumber(player.mechanics)}
+      </span>
+
+      <span className={styles.playerStat}>
+        Safety: {styleStatNumber(player.safety)}
+      </span>
+
+      <span className={styles.playerStat}>
+        Awareness: {styleStatNumber(player.awareness)}
+      </span>
+
+      <span className={styles.playerStat}>
+        Leadership: {styleStatNumber(player.leadership)}
+      </span>
+
+      {/* TODO: Work on player traits */}
+      {/* <span>
+        {
+          player.traits && player.traits.length > 0 && 
+          player.traits.map(t => )
+        }
+      </span> */}
     </div>
   );
 };
