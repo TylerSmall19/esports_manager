f1860d7877dcc0f33a37f7abfcf90f14b3032ef3
tylersmall19 <tylersmall.user@gmail.com>
2021-08-08 01:30:22 -0500

    Add the new player generator to the API

--- manager_api/src/index.ts
+++ manager_api/src/index.ts
@@ -1,11 +1,22 @@
 import express from "express";
+// @ts-ignore
+import cors from 'cors';
+import { generatePlayers } from "./services/playerService";
+
 const app = express();
+
+app.use(cors());
+
 const port = 8080; // default port to listen
 
 // define a route handler for the default home page
-app.get( "/", ( req: any, res: any ) => {
+app.get( "/", ( req, res) => {
     res.send( "Hello world!" );
 } );
+
+app.get('/players/scoutingList', ( req, res ) => {
+    res.json(generatePlayers(10));
+});
 
 // start the Express server
 app.listen( port, () => {
