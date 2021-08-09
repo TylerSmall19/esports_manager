caf1fba661694459c67128d0b421b452a8790cd4
tylersmall19 <tylersmall.user@gmail.com>
2021-08-08 21:50:41 -0500

    Change new Org to match the shape of Org

--- manager_platform/src/components/teamCreation/NewOrgSignup.tsx
+++ manager_platform/src/components/teamCreation/NewOrgSignup.tsx
@@ -1,31 +1,30 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { useState } from 'react';
 
 const NewOrgSignup = () => {
-  const [isSubmitted, setIsSubmitted] = useState(false);
-
   const handleOrgSubmit = (vals : any) => (e : Event) => {
     e.preventDefault();
 
-    console.log(vals, e);
+    console.log(vals);
   }
 
   return (
     <div>
       <h3>First, let's get some basics about your Org</h3>
       <br />
-      {!isSubmitted ? <NewOrgDetails onSubmit={handleOrgSubmit} /> : null }
+      <NewOrgDetails onSubmit={handleOrgSubmit} />
     </div>
   );
 };
 
 type FormValues = {
-  orgName: string;
+  name: string;
   ownerName: string;
+  tag: string;
 }
 
 const NewOrgDetails = (props : any) => {
   const { onSubmit } = props;
-  const initialState : FormValues = { orgName: '', ownerName: '' };
+  const initialState : FormValues = { name: '', ownerName: '', tag: '' };
 
   const [formState, setFormState] = useState<FormValues>(initialState);
 
@@ -36,9 +35,9 @@ const NewOrgDetails = (props : any) => {
   return (
     <form onSubmit={onSubmit(formState)}>
       <div>
-        <label className='inputs' htmlFor='orgName'>Organization Name</label> 
+        <label className='inputs' htmlFor='name'>Organization Name</label> 
         <br />
-        <input id='orgName' type='text' name='orgName' value={formState.orgName || ''} onChange={setValue} />
+        <input id='name' type='text' name='name' value={formState.name || ''} onChange={setValue} />
       </div>
 
       <br />
@@ -47,6 +46,14 @@ const NewOrgDetails = (props : any) => {
         <label className='inputs' htmlFor='ownerName'>Owner's Name (this will appear to users)</label>
         <br />
         <input id='ownerName' name='ownerName' type='text' value={formState.ownerName || ''} onChange={setValue} />
+      </div>
+
+      <br />
+
+      <div>
+        <label className='inputs' htmlFor='tag'>Org Tag</label>
+        <br />
+        <input id='tag' name='tag' type='text' value={formState.tag || ''} onChange={setValue} />
       </div>
 
       <br />
