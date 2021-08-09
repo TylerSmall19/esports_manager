2d1fe370011de9df21877decb46da9c5a5e11f91
tylersmall19 <tylersmall.user@gmail.com>
2021-08-09 00:26:47 -0500

    Added a post and get method to fetch org and add players

--- manager_api/src/index.ts
+++ manager_api/src/index.ts
@@ -1,27 +1,50 @@
 import express from "express";
 // @ts-ignore
 import cors from 'cors';
-import { ScoutablePlayersDatabaseService } from "./services/scoutablePlayerDatabaseService";
-import { config } from "dotenv";
+import { ScoutablePlayersDatabaseService } from "./services/database/scoutablePlayerDatabaseService";
+import { config as configEnv } from "dotenv";
+import { OrgDatabaseService } from "./services/database/orgDatabaseService";
+import { ObjectId } from "mongodb";
 
-config();
+configEnv();
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 const port = 8080; // default port to listen
-
-// define a route handler for the default home page
-app.get( "/", ( req, res) => {
-    res.send( "Hello world!" );
-} );
+const errorCode = 400;
 
 app.get('/players/scoutingList', async ( req, res ) => {
     const db = new ScoutablePlayersDatabaseService();
     const dbRes = await db.getScoutablePlayers();
 
     res.json(dbRes);
+});
+
+app.post('/orgs', async (req, res) => {
+    const db = new OrgDatabaseService();
+    const dbRes = await db.saveNewOrg(req.body);
+
+    if (dbRes)
+        res.status(201);
+    else
+        res.status(errorCode);
+
+    res.send();
+});
+
+app.post('/orgs/:orgId/players', async (req, res) => {
+    const db = new OrgDatabaseService();
+    const dbRes = await db.addPlayersToRoster(req.params.orgId, req.body.players);
+
+    if (dbRes)
+        res.status(201)
+    else
+        res.status(errorCode);
+
+    res.send();
 });
 
 // start the Express server
