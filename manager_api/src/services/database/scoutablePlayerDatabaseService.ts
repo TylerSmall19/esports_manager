ab73afec6bb67adcf62fd281ee36dbddc79be3e5
tylersmall19 <tylersmall.user@gmail.com>
2021-08-08 22:46:18 -0500

    Added finally; fixed issue causing the DB to return 0 results

--- manager_api/src/services/database/scoutablePlayerDatabaseService.ts
+++ manager_api/src/services/database/scoutablePlayerDatabaseService.ts
@@ -1,14 +1,13 @@
-import { InsertManyResult, InsertOneResult } from "mongodb";
 import { PlayerInfo } from "../../types/playerInfo";
 import { BaseDbService } from "./baseDbService";
 
 class ScoutablePlayersDatabaseService extends BaseDbService{
-
+  
   constructor () {
     super()
 
-    this.collectionName = 'NPCPlayers';
-    this.dbName = 'players';
+    this.dbName = 'NPCPlayers';
+    this.collectionName = 'players';
   }
 
   public async saveNewPlayers(players: PlayerInfo[]) : Promise<boolean> {
@@ -24,11 +23,13 @@ class ScoutablePlayersDatabaseService extends BaseDbService{
         let res = await collection.insertMany(players);
 
         return (this._wasSuccess(res));
-        // throw new Error(`Could not insert user: ${JSON.stringify(players)}`);
       }
       catch (err) {
         this.logger.logError(`Error saving: ${JSON.stringify(err)}`);
         return null;
+      }
+      finally {
+        this.databaseClient.close();
       }
   }
 
@@ -39,12 +40,14 @@ class ScoutablePlayersDatabaseService extends BaseDbService{
       const collection = this.databaseClient.db(this.dbName).collection(this.collectionName);
       var res = await collection.find<PlayerInfo>({ scoutable: true }).toArray();
 
-      if (res.length > 0)
-        return res;
+      return res;
     }
     catch (err) {
       this.logger.logError(`Error saving: ${JSON.stringify(err)}`);
       return null;
+    }
+    finally {
+      this.databaseClient.close();
     }
   }
 }
