3110a962b78091dd58fbb4c76350db3e16cbf032
tylersmall19 <tylersmall.user@gmail.com>
2021-08-08 22:46:29 -0500

    Write a service to save orgs

--- manager_api/src/services/database/orgDatabaseService.ts
+++ manager_api/src/services/database/orgDatabaseService.ts
@@ -1,12 +1,31 @@
 import { BaseDbService } from "./baseDbService";
+import { Org } from "../../types/org";
 
 export class OrgDatabaseService extends BaseDbService{
 
   constructor () {
     super()
 
-    this.collectionName = 'UserControlled';
-    this.dbName = 'orgs';
+    this.dbName = 'UserControlled';
+    this.collectionName = 'orgs';
   }
 
+  async saveNewOrg (org: Org) : Promise<boolean> {
+    try {
+      await this.databaseClient.connect();
+      const collection = this.databaseClient.db(this.dbName).collection(this.collectionName);
+
+      org = this._addCreatedDates(org);
+      const result = await collection.insertMany([org]);
+
+      return(this._wasSuccess(result));
+    }
+    catch (err) {
+      this.logger.logError(`Error saving: ${JSON.stringify(err)}`);
+      return null;
+    }
+    finally {
+      this.databaseClient.close();
+    }
+  }
 }
